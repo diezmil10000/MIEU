3184d6281c34c8f2ed00fc6baeb573b5f7e71b5e
D3st7 <marcalhdel@gmail.com>
2023-05-13 20:02:47 +0200

    Menú Ajustes (placeholder)
    +Volumen ajustable
        -Se puede cambiar toda la música
        -Solo la música/solo los efectos
        -Botón mutear efectos/mutear
         música
    
    +Brillo ajustable
    
    +Resolución de pantalla ajustable
    
    +Cámara sigue al jugador

---
+++ 2D platformer/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource _musicSource, _effectsSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip) 
+    {
+        _effectsSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMasterVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+
+    public void ChangeMusicVolume(float value)
+    {
+        _musicSource.volume = value;
+    }
+
+    public void ChangeEffectsVolume(float value)
+    {
+        _effectsSource.volume = value;
+    }
+
+    public void ToggleMusic()
+    {
+        _musicSource.mute = !_musicSource.mute;
+    }
+
+    public void ToggleEffects()
+    {
+        _effectsSource.mute = !_effectsSource.mute;
+    }
+
+
+
+}
--- 2D platformer/Assets/Scripts/mainMenu.cs
+++ 2D platformer/Assets/Scripts/mainMenu.cs
@@ -6,15 +6,41 @@ using UnityEngine.SceneManagement;
 public class mainMenu : MonoBehaviour
 {
 
+    public GameObject settings;
+    public GameObject menu;
+
+
     public void Jugar() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Ajustes()
+    {
+        irAjustes();
     }
 
     public void Salir()
     {
         Debug.Log("Salir");
         Application.Quit();
+    }
+
+    public void Volver() 
+    {
+        irAtras();
+    }
+
+    private void irAtras()
+    {
+        settings.SetActive(false);
+        menu.SetActive(true);
+    }
+
+    private void irAjustes()
+    {
+        settings.SetActive(true);
+        menu.SetActive(false);
     }
 
 }
