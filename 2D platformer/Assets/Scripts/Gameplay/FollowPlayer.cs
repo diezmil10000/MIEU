ccd545b391a7184ad749f47308a14ac6ce86b629
D3st7 <marcalhdel@gmail.com>
2023-06-02 23:49:45 +0200

    Cámara en proceso
    +La cámara se detiene al borde de las escaleras y de la pared de la sala derecha

--- 2D platformer/Assets/Scripts/Gameplay/FollowPlayer.cs
+++ 2D platformer/Assets/Scripts/Gameplay/FollowPlayer.cs
@@ -9,9 +9,22 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField] private float y = 2.0f;
     [SerializeField] private float z = -2.5f;
 
+    public Vector3 offset;
+
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, y-0.15f, z);
+        //Seguir al jugador
+        if (player.position.x >= -82.5 && player.position.x <= 71)
+        {
+            transform.position = player.transform.position + new Vector3(0, y - 0.15f, z);
+        }
+        else
+        {
+            Vector3 position = transform.position;
+            //Cojo la posición del jugador en y, además le sumo una pequeña corrección para que la cámara no quede muy baja.
+            position.y = (player.position + offset).y +1.85f; 
+            transform.position = position;
+        }
     }
 }
